76d47f44a58a0656dace7cdb73df2ef707262acb
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2023-08-02 13:01:58 +0200

    Create keys.py

---
+++ code/keys.py
@@ -0,0 +1,16 @@
+"""
+This file contains your MySQL API keys
+
+    Replace <password> with your password
+    Keep the quotation marks
+
+"""
+
+# keys for MySQL database
+config = {
+    'user': 'root',
+    'password': '<password>',
+    'host': '127.0.0.1',
+    'database': 'db-ecommerce',
+    'raise_on_warnings': True
+}
