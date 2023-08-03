bd71dab32c9592c1e1ffcb2f626f213f38b046df
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2023-08-03 09:31:57 +0200

    Update data.sql

--- code/data.sql
+++ code/data.sql
@@ -1,11 +1,3 @@
-CREATE TABLE ecommerce_data (
-    id INT AUTO_INCREMENT,
-    eshop_name VARCHAR(255),
-    date DATE,
-    annual_revenue DECIMAL(10, 2),
-    active_user_base DECIMAL(10, 2),
-    average_rating DECIMAL(3, 1),
-    social_media_followers INT,
-    average_response_time DECIMAL(3, 2),
-    PRIMARY KEY(id)
-);
+DROP DATABASE IF EXISTS `db_ecommerce`;
+CREATE DATABASE `db_ecommerce`; 
+USE `db_ecommerce`;
